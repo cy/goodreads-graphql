4e1b15e62d9f8ea367f7ecb5f72b66016e9ebbaa
Christine Yu <christine.tc.yu@gmail.com>
2016-10-15 23:25:25 -0400

    readme

---
+++ README.md
@@ -0,0 +1,3 @@
+Graphql wrapper over the 📗 Goodreads API
+
+GOODREADS_API_KEY={key} FLASK_APP=schema.py flask run
--- schema.py
+++ schema.py
@@ -2,9 +2,6 @@ from flask import Flask
 from flask_graphql import GraphQLView
 import graphene
 import requests
-import xmltodict, json
-from xmljson import parker
-from lxml.etree import fromstring, tostring
 from bs4 import BeautifulSoup
 from os import environ as env
 
@@ -16,7 +13,6 @@ class Book(graphene.ObjectType):
     id = graphene.String()
     title = graphene.String()
     average_rating = graphene.Int()
-    author = graphene.String()
 
 
 class Review(graphene.ObjectType):
@@ -25,19 +21,36 @@ class Review(graphene.ObjectType):
     book = graphene.Field(Book)
     rating = graphene.Int(description="Rate book out of 5, 1 - throw out window 5 - the author is my prophet")
 
+
 class Query(graphene.ObjectType):
     reviews = graphene.List(Review)
     user_reviews = graphene.List(Review, user_id=graphene.ID())
     review = graphene.Field(
         Review,
         id=graphene.ID(),
-        description='Just one reivew belonging to an user',
     )
 
 
     def resolve_reviews(self, args, context, info):
-        r = requests.get('https://www.goodreads.com/review/recent_reviews.xml?key={}'.format(API_KEY))
-        soup = BeautifulSoup(r.text, 'xml')
+        response = requests.get('https://www.goodreads.com/review/recent_reviews.xml?key={}'.format(API_KEY))
+        soup = BeautifulSoup(response.text, 'xml')
+        reviews = soup.find_all('review')
+        return [Review(
+            user_id = r.user.id.get_text(),
+            id = r.id.get_text(),
+            rating = r.rating.get_text(),
+            book = Book(
+                title = r.book.title.get_text(),
+                id = r.book.id.get_text(),
+                average_rating = r.book.average_rating.get_text(),
+            )
+        ) for r in reviews]
+
+
+    def resolve_user_reviews(self, args, context, info):
+        user_id = args.get('user_id')
+        request = requests.get('https://www.goodreads.com/review/list/{}.xml?key={}&v=2'.format(user_id, API_KEY))
+        soup = BeautifulSoup(request.text, 'xml')
         reviews = soup.find_all('review')
         return [Review(
             id = r.id.get_text(),
@@ -46,34 +59,25 @@ class Query(graphene.ObjectType):
                 title = r.book.title.get_text(),
                 id = r.book.id.get_text(),
                 average_rating = r.book.average_rating.get_text(),
-                author = [a.name for a in r.book.authors]
             )
         ) for r in reviews]
 
 
-    def resolve_user_reviews(self, args, context, info):
-        user_id = args.get('user_id')
-        r = requests.get('https://www.goodreads.com/review/list/{}.xml?key={}&v=2'.format(user_id, API_KEY))
-        soup = BeautifulSoup(r.text, 'xml')
-        reviews = soup.find_all('review')
-        return [Review(
-            id = r.id.get_text(),
-            rating = r.rating.get_text(),
-            book = r.book.title.get_text(),
-            ) for r in reviews]
-
-
     def resolve_review(self, args, context, info):
         id = args.get('id')
-        r = requests.get('https://www.goodreads.com/review/show.xml?id={}&key={}'.format(id, API_KEY))
-        soup = BeautifulSoup(r.text, 'xml')
-        review = soup.review
+        request = requests.get('https://www.goodreads.com/review/show.xml?id={}&key={}'.format(id, API_KEY))
+        soup = BeautifulSoup(request.text, 'xml')
+        r= soup.review
         return Review(
-                id = review.id.get_text(),
-                user_id = review.user.id.get_text(),
-                rating = review.rating.get_text(),
-                book = review.book.title.get_text(),
-                )
+                id = r.id.get_text(),
+                user_id = r.user.id.get_text(),
+                rating = r.rating.get_text(),
+                book = Book(
+                title = r.book.title.get_text(),
+                id = r.book.id.get_text(),
+                average_rating = r.book.average_rating.get_text(),
+            )
+        )
 
 
 schema = graphene.Schema(query=Query)
